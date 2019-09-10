3fa6773e05af676df0ac13b24691e1e3d58058e1
kiririnshi <49988006+kiririnshi@users.noreply.github.com>
2019-09-10 09:50:57 -0300

    Add files via upload

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,6 +7,15 @@ import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 
+import Drawer from '@material-ui/core/Drawer';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
+
 const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1,
@@ -19,20 +28,79 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
+
+
+
 export default function ButtonAppBar() {
   const classes = useStyles();
+
+  /*
+  const setState = React.useState({
+    left: false,
+  });
+
+  const toggleDrawer = (open) => event => {
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+  
+    setState({open});
+  };
+
+  */
+  const [open, setOpen] = React.useState(false);
+
+  function handleDrawerOpen() {
+   setOpen(true);
+  }
+
+  function handleDrawerClose() {
+     setOpen(false);
+  }
+
+  const sideList = side => (
+    <div
+      className={classes.list}
+      role="presentation"
+      onClick={handleDrawerClose}
+      onKeyDown={handleDrawerClose}
+    >
+      <List>
+        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {['All mail', 'Trash', 'Spam'].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  );
 
   return (
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon />
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu"
+          onClick={handleDrawerOpen}>
+          <Drawer open={open} onClose={handleDrawerClose}>
+            {sideList()}
+          </Drawer>
+          <MenuIcon/>
           </IconButton>
           <Typography variant="h6" className={classes.title}>
             Menu
           </Typography>
           <Button color="inherit">Login</Button>
+          
         </Toolbar>
       </AppBar>
     </div>
